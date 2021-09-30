bf5ec818836e73eb74cf5bc9f75599e64d51e772
Isaac Greentree <5greei12@solent.ac.uk>
2021-09-30 16:04:01 +0100

    added code to display characters

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+#displays escape characters
+print ("\n displays newline")
+print ("\t displays a tab space")
+print("\\ displays back slash")
+print ("\" displays a double quote")
+print ("\' displays a single quote")
+
