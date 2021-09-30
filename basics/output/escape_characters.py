a248735992f77ef7d94167eebbad36f0fc3b9c8a
Isaac Greentree <5greei12@solent.ac.uk>
2021-09-30 16:07:04 +0100

    added i amj beep to escape characters

--- basics/output/escape_characters.py
+++ basics/output/escape_characters.py
@@ -4,4 +4,8 @@ print ("\t displays a tab space")
 print("\\ displays back slash")
 print ("\" displays a double quote")
 print ("\' displays a single quote")
+print ("\n")
+print("\t I am Beep!")
+
+
 
