8b971868c9b707379d42380a9ac27ad92a0f18ef
Isaac Greentree <5greei12@solent.ac.uk>
2021-10-07 15:35:01 +0100

    added if else file

---
+++ basics/decisions/if_else.py
@@ -0,0 +1,8 @@
+print ("Please enter activity to be performed,")
+task = input ()
+if task == 'calculate':
+    print("Performing calculations...")
+else: print("performing activity")
+print()
+print("activity completed!")
+
