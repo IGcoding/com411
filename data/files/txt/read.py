ba695f56fd5338841bd641d5693ea2e9271b5e4d
Isaac Greentree <5greei12@solent.ac.uk>
2021-10-21 16:41:53 +0100

    created ready.py

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,3 +1,8 @@
-with open ("library.txt") as file:
-    data = file.read()
-    lines = data.split ('\n')
+def display_chars():
+    with open ("library.txt") as file:
+        data = file.read()
+
+
+
+
+display_chars()
