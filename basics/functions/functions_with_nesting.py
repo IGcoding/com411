e472c574f52276bc5d0c17d1b6595fe2096fc2ee
Isaac Greentree <5greei12@solent.ac.uk>
2021-10-21 13:34:31 +0100

    added functions with nesting

---
+++ basics/functions/functions_with_nesting.py
@@ -0,0 +1,9 @@
+# creates a defined function called identity
+def identify():
+    print ("What do you see in front of you?")
+    input ()
+    if input == "a large bolder":
+            print ("Its time to run!")
+    else:
+            print ("We will be fine")
+identify()
