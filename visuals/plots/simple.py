02e0f9c83bb064f4b188b8fc57ed927c5cff05f3
Isaac Greentree <5greei12@solent.ac.uk>
2021-11-11 16:46:49 +0000

    changed simple plot

--- visuals/plots/simple.py
+++ visuals/plots/simple.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-x = [0, 5, 10]
-y = [0, 50, 100]
+x = [0, 6, 8, 9.5, 12, 19, 33, 22, 44, 56]
+y = [0, 30, 41, 50, 53, 57, 60, 75, 90, 100]
 
-plt.plot(x, y)
+plt.plot(x, y, 'o')
 plt.show()
